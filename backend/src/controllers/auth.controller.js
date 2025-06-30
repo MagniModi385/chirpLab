31144fd696a72fe2c11ed43e57543de2ab2db10b
Chinmay Modi <chinmaymodi1357@Chinmays-MacBook-Air.local>
2025-06-30 16:05:00 +0530

    added new files

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
     import User from "../models/User.js";
-    import jwt from "jsonwebtoken"
-
+    import jwt from "jsonwebtoken";
+    import {upsertStreamUser} from "../lib/stream.js";
     export async function signup(req,res)
     {
     const{email,password,fullName}=req.body;
@@ -24,13 +24,26 @@
                 return res.status(400).json({message:"Email already exists"});
 
             }
+
         const ind=Math.floor(Math.random()*100)+1;
         const randomAvatar=`https://avatar.iran.liara.run/public/${ind}.png`;
         const newUser=await User.create({
             email,fullName,password,
             profilePic:randomAvatar,
         })
-        const token=jwt.sign({userId:newUser._Id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'});
+        //connection with streamchat
+        try {
+              await upsertStreamUser({
+            id:newUser._id.toString(),
+            name:newUser.fullName,
+            image:newUser.profilePic || "",
+        });
+        console.log(`Stream user created for ${newUser.fullName}`);
+        } catch (error) {
+            console.log("Error in creating stream user: ",error)
+        }
+      //cookie generation
+        const token=jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'});
         res.cookie("jwt",token,
             {
                 maxAge: 7*24*60*60*1000,
@@ -58,7 +71,8 @@
        if(!user) return res.status(401).json({message:"Invalid email or password"});
        const isPasswordCorrect=await user.matchPassword(password);
        if(!isPasswordCorrect) return res.status(401).json({message:"Invalid email or password"});
-        const token=jwt.sign({userId:user._Id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'});
+
+        const token=jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'});
         res.cookie("jwt",token,
             {
                 maxAge: 7*24*60*60*1000,
@@ -78,4 +92,34 @@
     {
         res.clearCookie("jwt");
         res.status(200).json({success:true,message:"Logout successfull"});
+    }
+    export async function onboard(req,res)
+    {
+        try {
+            const userId=req.user._id;
+            const{fullName,bio,nativeLanguage,learningLanguage,location}=req.body;
+            if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location)
+                {
+                    return res.status(400).json({
+                    message:"All fields are required",
+                    missingFields:
+                    [
+                        !fullName && "fullName",
+                        !bio && "bio",
+                        !nativeLanguage && "nativeLanguage",
+                        !learningLanguage &&"learningLanguage",
+                        !location && "location",
+                    ],
+                    });
+                }
+          const updatedUser=await User.findByIdAndUpdate(userId,
+                {
+                    ...req.body,isOnboarded:true,
+                },{new:true});
+                if(!updatedUser) return res.status(404).json({message:"User not found"});
+                //todo update user info in stream chat
+        } catch (error) {
+            
+        }
+
     }
